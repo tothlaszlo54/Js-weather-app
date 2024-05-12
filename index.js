834a2c11e561f9110196cbfe9659553b986efcc5
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-05-12 15:59:30 +0200

    The Event listener has been created to weatherform.

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "9c788ababc7e0092f06f0112506ad171";
 
+weatherForm.addEventListener("submit", async event => {
+
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else{
+        displayError("Please enter a city");
+    }
+});
