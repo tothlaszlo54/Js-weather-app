6a2125bf41bf42c5804c2aa6ffa765ab47ff9e99
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-05-12 16:02:47 +0200

    The getWeatherEmoji function has been created.

--- index.js
+++ index.js
@@ -72,3 +72,27 @@ function displayWeatherInfo(data){
     card.appendChild(descDisplay);
     card.appendChild(weatherEmoji);
 }
+
+function getWeatherEmoji(weatherId){
+
+    switch(true){
+        case (weatherId >= 200 && weatherId < 300):
+            return "⛈️";
+        case (weatherId >= 300 && weatherId < 400):
+            return "🌧️";
+        case (weatherId >= 500 && weatherId < 600):
+            return "🌧️";
+        case (weatherId >= 600 && weatherId < 700):
+            return "❄️";
+        case (weatherId >= 700 && weatherId < 800):
+            return "🌫️";
+        case (weatherId === 800):
+            return "☀️";
+        case (weatherId >= 801 && weatherId < 810):
+            return "☁️";
+        default:
+            return "❓";
+    }
+}
+
+
