a7b2eee44c8eca51f0486cae8040fbf23ddaec5f
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-05-12 15:58:14 +0200

    The api key and the necessary variables have been created.

---
+++ index.js
@@ -0,0 +1,7 @@
+// WEATHER APP
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "9c788ababc7e0092f06f0112506ad171";
+
